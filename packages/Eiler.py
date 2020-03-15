f4731065479a56cd57008adaadfe0a315e3f155f
Lesh4 <47988759+Lesh4@users.noreply.github.com>
2020-03-15 22:03:24 +0300

    Update Eiler.py
    при недопустимом вычислении в res добавляется слово error

--- packages/Eiler.py
+++ packages/Eiler.py
@@ -17,12 +17,15 @@ from packages.methods_funcs import zamena, check_znaki
 
 def Eiler(x, x_last, y, h, uravn):
     res = []
-    kol = '.' + str(check_znaki(x, x_last, y, h)+1) + 'f'
-    while x <= x_last:
-        uravn_res = eval(zamena(x, y, uravn))
-        y_del = h*uravn_res
-        res.append([float(format(x, kol)), float(format(y, kol)), float(format(uravn_res, kol)), float(format(y_del, kol))])
-        y += y_del
-        x += h
+    try:
+        kol = '.' + str(check_znaki(x, x_last, y, h)+1) + 'f'
+        while x <= x_last:
+            uravn_res = eval(zamena(x, y, uravn))
+            y_del = h*uravn_res
+            res.append([float(format(x, kol)), float(format(y, kol)), float(format(uravn_res, kol)), float(format(y_del, kol))])
+            y += y_del
+            x += h
+    except:
+        res.append("Error")
     return res
 
