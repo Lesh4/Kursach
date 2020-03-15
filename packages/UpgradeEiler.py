5442e4c3d269275851b7909c2158cfb955225a74
Lesh4 <47988759+Lesh4@users.noreply.github.com>
2020-03-15 22:03:34 +0300

    Update UpgradeEiler.py
    при недопустимом вычислении в res добавляется слово error

--- packages/UpgradeEiler.py
+++ packages/UpgradeEiler.py
@@ -19,12 +19,15 @@ from packages.methods_funcs import zamena, check_znaki
 
 def UpgradeEiler(x, x_last, y, h, uravn):
     res = []
-    kol = '.' + str(check_znaki(x, x_last, y, h)+1) + 'f'
-    while x <= x_last:
-        y_shtrih = eval(zamena(x, y, uravn))*(h/2)
-        uravn_res = eval(zamena(x+h/2,y+y_shtrih , uravn))
-        y_del = uravn_res * h
-        res.append([float(format(x, kol)), float(format(y, kol)), float(format(uravn_res, kol)), float(format(y_del, kol))])
-        y += y_del
-        x += h
+    try:
+        kol = '.' + str(check_znaki(x, x_last, y, h)+1) + 'f'
+        while x <= x_last:
+            y_shtrih = eval(zamena(x, y, uravn))*(h/2)
+            uravn_res = eval(zamena(x+h/2,y+y_shtrih , uravn))
+            y_del = uravn_res * h
+            res.append([float(format(x, kol)), float(format(y, kol)), float(format(uravn_res, kol)), float(format(y_del, kol))])
+            y += y_del
+            x += h
+    except:
+        res.append("Error")
     return res
