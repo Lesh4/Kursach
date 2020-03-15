7be77f979576395f94e82cecbda290e01edbc7a8
Lesh4 <47988759+Lesh4@users.noreply.github.com>
2020-03-15 17:10:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -71,28 +71,28 @@ class Menu(tk.Frame):
 
     def create_widgets_start(self):
         tk.Label(self.start, text = "Решение дифференциальных уравнений", font=("Segoe print", 18), bg = "#F5F5DC").place(x=120, y=30)
-        x_lb = tk.Label(self.start, text = "X начальное", font=("Segoe print", 14), bg = "#F5F5DC").place(x=30, y=100)
+        tk.Label(self.start, text = "X начальное", font=("Segoe print", 14), bg = "#F5F5DC").place(x=30, y=100)
         self.entry_start_x = tk.Entry(self.start, width=10)
         self.entry_start_x.place(x=190, y =110)
 
-        x_lb = tk.Label(self.start, text = "X конечное", font=("Segoe print", 14), bg = "#F5F5DC").place(x=400, y=100)
+        tk.Label(self.start, text = "X конечное", font=("Segoe print", 14), bg = "#F5F5DC").place(x=400, y=100)
         self.entry_last_x = tk.Entry(self.start, width=10)
         self.entry_last_x.place(x=560, y =110)
 
-        y_lb = tk.Label(self.start, text = "Y начальное", font=("Segoe print", 14), bg = "#F5F5DC").place(x=30, y=140)
+        tk.Label(self.start, text = "Y начальное", font=("Segoe print", 14), bg = "#F5F5DC").place(x=30, y=140)
         self.entry_y = tk.Entry(self.start, width=10)
         self.entry_y.place(x=190, y=150)
 
-        h_lb = tk.Label(self.start, text = "Шаг", font=("Segoe print", 14), bg = "#F5F5DC").place(x=430, y=140)
+        tk.Label(self.start, text = "Шаг", font=("Segoe print", 14), bg = "#F5F5DC").place(x=430, y=140)
         self.entry_h = tk.Entry(self.start, width=10)
         self.entry_h.place(x=560, y=150)
 
-        func_lb = tk.Label(self.start, text="Функция вида: y` = f(x;y)", font=("Segoe print", 14), bg = "#F5F5DC").place(x=30, y=240)
+        tk.Label(self.start, text="Функция вида: y` = f(x;y)", font=("Segoe print", 14), bg = "#F5F5DC").place(x=30, y=240)
         self.entry_func = tk.Entry(self.start, width=35)
         self.entry_func.place(x=45, y = 300, height=50)
 
-        Eiler = tk.Button(self.start, text="Метод Эйлера", command=self.start_Eiler, font=("Segoe print", 11), bg = "#FAEBD7").place(x=500, y=240)
-        UpgradeEiler = tk.Button(self.start, text="Усовершенствованный метод Эйлера", command=self.start_UpgradeEiler, font=("Segoe print", 11), bg = "#FAEBD7").place(x=400, y=300)
+        tk.Button(self.start, text="Метод Эйлера", command=self.start_Eiler, font=("Segoe print", 11), bg = "#FAEBD7").place(x=500, y=240)
+        tk.Button(self.start, text="Усовершенствованный метод Эйлера", command=self.start_UpgradeEiler, font=("Segoe print", 11), bg = "#FAEBD7").place(x=400, y=300)
 
     def create_widgets_EilerWindow(self):
         tk.Label(self.EilerWindow, text = "Метод Эйлера", font=("Segoe print", 18), bg = "#F5F5DC").place(x=270, y=30)
