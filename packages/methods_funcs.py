a89de34375866c49dcd58207168bda9d8146c3af
Lesh4 <sli2001@mail.ru>
2020-04-24 01:02:31 +0300

    Update methods_funcs.py
    Добавлены комменты к функциям и модулю methods_funcs

--- packages/methods_funcs.py
+++ packages/methods_funcs.py
@@ -1,9 +1,18 @@
+"""
+Модуль вспомогательных функций для методов решения.
+"""
 def check_znaki(x, x_last, y, h):
-	string = [str(x), str(x_last), str(y), str(h)]
-	kol = [len(s.split('.')[1]) for s in string]
-	return max(kol)
+    """
+    Возвращает количество знаков, которое должно быть после запятой у результата.
+    """
+    string = [str(x), str(x_last), str(y), str(h)]
+    kol = [len(s.split('.')[1]) for s in string]
+    return max(kol)
 
 def zamena(x, y, uravn):
+    """
+    Заменяет строковые значения x и y на значения float.
+    """
     uravn = uravn.replace('y', str(y))
     uravn = uravn.replace('x', str(x))
     uravn = uravn.replace('^', '**')
