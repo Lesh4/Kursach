30360fcfdbfec994ba3a839f6f6393ac34faac5b
Lesh4 <sli2001@mail.ru>
2020-04-24 01:05:38 +0300

    Update Eiler.py

--- packages/Eiler.py
+++ packages/Eiler.py
@@ -15,16 +15,19 @@ from math import *
 from packages.methods_funcs import zamena, check_znaki
 
 def Eiler(x, x_last, y, h, uravn):
+    """
+    Решение методом Эйлера.
+    """
     res = []
     try:
         kol = '.' + str(check_znaki(x, x_last, y, h)+1) + 'f'
         while x <= x_last:
             uravn_res = eval(zamena(x, y, uravn))
-            y_del = h*uravn_res
-            res.append([float(format(x, kol)), float(format(y, kol)), float(format(uravn_res, kol)), float(format(y_del, kol))])
+            y_del = h * uravn_res
+            res.append([float(format(x, kol)), float(format(y, kol)),
+                        float(format(uravn_res, kol)), float(format(y_del, kol))])
             y += y_del
             x += h
     except:
         res.append("Error")
     return res
-
